2af81ae80cfc21a82fa306da8e92d387aa4ec56b
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-29 22:29:18 -0400

    reviewing react basics and also trying to learn more so I can better think of more concise ways to approach react in my next project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
+        <Person name="Sean" age="26"/>
+        <Person name="Katherine" age="24"/>
+        <Person name="Sean" age="26">
+          My Hobbies : racing
+        </Person>
+        <Person name="Doone" age="20"/>
       </div>
     );
   }
