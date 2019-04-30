2f88b358294390ec241b4271da6566c3921b262a
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-29 23:05:21 -0400

    basic concepts of state, just review but it's good to reinforce the basics

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import Person from "./Person/Person"
 
 class App extends Component {
+    state = {
+      persons : [
+        {name: "Sean", age: 26},
+        {name: "Katherine", age: 24},
+        {name: "Doone", age: 28},
+      ]
+    }
+
+    switchNameHandler = ()=>{
+      console.log("CLICKED")
+    }
+
   render(){
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
-        <Person name="Sean" age="26"/>
-        <Person name="Katherine" age="24"/>
-        <Person name="Sean" age="26">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
           My Hobbies : racing
         </Person>
-        <Person name="Doone" age="20"/>
       </div>
     );
   }
