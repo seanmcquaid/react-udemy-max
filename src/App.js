fcdb94ff9e863130d320dbb79cbb6cac2667ab87
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-01 19:19:41 -0400

    learned about dynamic styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import { publicDecrypt } from 'crypto';
+import { whileStatement } from '@babel/types';
 
 class App extends Component {
   state = {
@@ -47,7 +48,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor : "white",
+      backgroundColor : "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -70,12 +72,21 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = "red";
+    }
+
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push("red");
+    }
+    if(this.state.persons.length <= 1){
+      classes.push("bold")
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button
         style={style} 
         onClick={this.togglePersonsHandler}>
