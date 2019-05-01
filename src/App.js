2ab0dea1fa9bc2c440a5c77d502adf223f8ff308
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-01 14:34:11 -0400

    turns out my initial way of doing conditional rendering is solid

--- src/App.js
+++ src/App.js
@@ -51,17 +51,10 @@ class App extends Component {
       cursor: "pointer"
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-        style={style} 
-        onClick={this.togglePersonsHandler}>
-        Switch Name
-        </button>
-        { 
-        this.state.showPersons === true?
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
         <div>
           <Person
             name={this.state.persons[0].name}
@@ -79,8 +72,20 @@ class App extends Component {
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
           />
-        </div> : null
-        }
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+        style={style} 
+        onClick={this.togglePersonsHandler}>
+        Switch Name
+        </button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
