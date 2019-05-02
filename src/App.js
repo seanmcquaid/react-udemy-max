1050e8156c89c6e2a2747ca9e378d027e8d6f763
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-02 10:23:11 -0400

    learned about radium for in line styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import { publicDecrypt } from 'crypto';
-import { whileStatement } from '@babel/types';
+import Radium, {StyleRoot} from "radium";
 
 class App extends Component {
   state = {
@@ -53,7 +52,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover" : {
+        backgroundColor :"lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -73,7 +76,11 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = "red";
-    }
+      style[":hover"] = {
+        backgroundColor : "salmon",
+        color: "black"
+      };
+    };
 
     const classes = [];
     if(this.state.persons.length <= 2){
@@ -84,19 +91,21 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button
-        style={style} 
-        onClick={this.togglePersonsHandler}>
-        Switch Name
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(" ")}>This is really working!</p>
+          <button
+          style={style} 
+          onClick={this.togglePersonsHandler}>
+          Switch Name
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
