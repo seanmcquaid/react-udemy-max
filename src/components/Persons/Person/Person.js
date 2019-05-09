c3a27211240a28bdbab6cbd803a369241319a999
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-09 16:31:40 -0400

    difference ways to use refs in react

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,29 @@ import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/WithClass";
 
 class Person extends Component {
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef()
+    }
+
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus()
+    }
+
     render(){
         console.log("person.js rendering")
     return (
         <Aux>
             <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
             <p key="i2">{this.props.children}</p>,
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>  
+            <input 
+                key="i3"
+                ref={this.inputElementRef}
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name}
+            />  
         </Aux>
         )
     }  
